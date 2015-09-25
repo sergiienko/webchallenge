b50290618092e4473887ecc672cb4561f22196f2
Dmytro G. Sergiienko <sergiienko.d.g@gmail.com>
2015-09-25 12:17:21 +0300

    Removed javascript for search button.

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function hasClass(obj, className) {
-	var classAttr = obj.getAttribute('class');
-	var classArr = classAttr.split(' ');
-	for(var i = 0; i < classArr.length; i++)
-		if (classArr[i] == className)
-			return true;
-	return false;
-}
